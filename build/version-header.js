9610db4d82e84b09ae05f0171eb6065fb113cd2c
Roy Jacobs <roy.jacobs@gmail.com>
2011-09-11 21:07:56 +0100

    Rebuilt after fix for array updates by jpc. Fixes #27

--- build/version-header.js
+++ build/version-header.js
@@ -1,4 +1,4 @@
-// Knockout Mapping plugin v2.0
+// Knockout Mapping plugin v2.0.1
 // (c) 2011 Steven Sanderson, Roy Jacobs - http://knockoutjs.com/
 // License: Ms-Pl (http://www.opensource.org/licenses/ms-pl.html)
 
