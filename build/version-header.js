f996b65ce93ccb934746fdb581dc2060359f9326
Roy Jacobs <roy.jacobs@gmail.com>
2012-02-16 12:04:33 +0100

    Merged Domenic Denicola's module wrapper, closes #51, closes #52

--- build/version-header.js
+++ build/version-header.js
@@ -1,4 +1,4 @@
-// Knockout Mapping plugin v2.0.4
+// Knockout Mapping plugin v2.1.0
 // (c) 2011 Steven Sanderson, Roy Jacobs - http://knockoutjs.com/
 // License: MIT (http://www.opensource.org/licenses/mit-license.php)
 
