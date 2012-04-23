748ce7a2c954fd85c7d3745c5721ea2ce44a6b6d
Roy Jacobs <roy.jacobs@gmail.com>
2012-04-23 16:32:10 +0200

    Added DEBUG conditional support

--- build/build-windows.bat
+++ build/build-windows.bat
@@ -1,8 +1,10 @@
 @echo off 
 set AllFiles=..\knockout.mapping.js
 
-copy /A /B version-header.js+%AllFiles% output\knockout.mapping-latest.debug.js
-
-@rem Now call Google Closure Compiler to produce a minified version
+@rem Call Google Closure Compiler to produce a minified version
 copy /y version-header.js output\knockout.mapping-latest.js
+cscript tools\searchReplace.js "var DEBUG=true;" "/**@const*/var DEBUG=false;" output\knockout.mapping-latest.debug.js >nul
 tools\curl -d output_info=compiled_code -d output_format=text -d compilation_level=SIMPLE_OPTIMIZATIONS --data-urlencode js_code@output\knockout.mapping-latest.debug.js "http://closure-compiler.appspot.com/compile" >> output\knockout.mapping-latest.js
+
+@rem Also create a simple debug-enabled version
+copy /A /B version-header.js+%AllFiles% output\knockout.mapping-latest.debug.js
